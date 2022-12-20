d439e5bad76bf4e011751c1ad05d66bf53ac6385
VSMent <vsmentvs@gmail.com>
2022-12-20 18:13:40 +0200

    basic components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import * as React from 'react';
-
 import './style.css';
 
+const HeaderWithCounter = ({ turn }: { turn: number }) => <h2>{turn}</h2>;
+const Battlefield = () => {
+  return <div></div>;
+};
+const ResetButton = ({ reset }: { reset: () => void }) => {
+  return (
+    <button type="button" onClick={reset}>
+      Reset
+    </button>
+  );
+};
+
 export default function App() {
-  return <div className="app">{'test'}</div>;
+  const reset = () => {
+    console.log('r');
+  };
+  return (
+    <div className="app">
+      <HeaderWithCounter turn={0} />
+      <Battlefield />
+      <ResetButton reset={reset} />
+    </div>
+  );
 }
